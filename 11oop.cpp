f9b01e302f97c95a58473467a1c9cc7fd82874cb
DlililS <ladus2004daria@gmail.com>
2022-05-12 22:06:45 +0300

    Initial commit

---
+++ 11oop.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Animal {
+public:
+    string animalname;
+    explicit Animal(string& name) :animalname(name)
+    {}
+    virtual ~Animal(){}
+
+public:
+    string Name() { return animalname; }
+};
+class Cat : public Animal
+{
+public:
+    explicit Cat(string& name):Animal(name) {}
+
+};
+
+class Dog : public Animal
+{
+public:
+    explicit Dog(string& name):Animal(name){}
+
+};
+class Parrot : public Animal {
+public:
+    explicit Parrot(string& name):Animal(name){}
+};
+int main()
+{
+    return 0;
+}
